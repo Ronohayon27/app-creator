1e6e13a9c241d41dece73756415c671ce8157d40
Ronohayon27 <ronohayon27@gmail.com>
2025-06-27 21:35:44 +0300

    enableing trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
-export default async function Home() {
-  return <div></div>;
+"use client";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
+export default function Home() {
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({ text: "Hello World" }));
+  return <div>{JSON.stringify(data)}</div>;
 }
