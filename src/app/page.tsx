835e05644a39f9c7edb65e18110fae9ed2df79e1
Ronohayon27 <ronohayon27@gmail.com>
2025-06-28 14:41:54 +0300

    add inngest for background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
+import { useMutation } from "@tanstack/react-query";
 export default function Home() {
   const trpc = useTRPC();
-  const { data } = useQuery(trpc.hello.queryOptions({ text: "Hello World" }));
-  return <div>{JSON.stringify(data)}</div>;
+  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+  return (
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button onClick={() => invoke.mutate({ text: "Ron" })}>
+        Invoke Background Function
+      </Button>
+    </div>
+  );
 }
