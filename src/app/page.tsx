8d1d127b945d8c1d0a4643043c362e6db378c3aa
Ronohayon27 <ronohayon27@gmail.com>
2025-07-07 19:32:05 +0300

    creating and saving msgs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,35 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
+import { toast } from "sonner";
+
 export default function Home() {
   const [value, setValue] = useState("");
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+  const { data: messages } = useQuery(trpc.messages.getMany.queryOptions());
+  const createMessage = useMutation(
+    trpc.messages.create.mutationOptions({
+      onSuccess: () => {
+        toast.success("Message created");
+      },
+    })
+  );
+
   return (
     <div className="p-4 max-w-7xl mx-auto">
       <Input value={value} onChange={(e) => setValue(e.target.value)} />
-      <Button onClick={() => invoke.mutate({ value: value })}>
+      <Button
+        disabled={!value || createMessage.isPending}
+        onClick={() => createMessage.mutate({ value: value })}
+      >
         Invoke Background Function
       </Button>
+      {messages?.map((message) => (
+        <div key={message.id}>{message.content}</div>
+      ))}
     </div>
   );
 }
